4a4473f922392a91a7984d67147ad597fc16c562
youngsam.lee <youngsam.lee@thingsflow.com>
2024-03-07 16:32:31 +0900

    fix:test

--- src/routes/index.js
+++ src/routes/index.js
@@ -14,6 +14,7 @@ const api = axios.create({
 router.get("/", async function (req, res, next) {
   try {
     console.log("Sending request to backend albums api");
+    console.log("배포 잘되는지 test github");
     var data = await api.get("/albums");
     console.log("Response from backend albums api: ", data.data);
     res.render("index", {
@@ -25,5 +26,11 @@ router.get("/", async function (req, res, next) {
     next(err);
   }
 });
+
+/* 웹화면에 process.env.vaulttest 를 찍어라 , 웹페이지에서 보이도록 찍으라 */
+router.get("/vaulttest", function (req, res) {
+  res.send(process.env.vaulttest);
+});
+console.log("vaulttest", process.env.vaulttest);
 
 module.exports = router;
